ed6a47a77e890df13d46b3bc3c6868a9b2d5072a
BUDDY_1cent <BUDDY_1cent@192.168.0.100>
2014-11-05 22:46:11 +0100

    new pong

--- pong/Deska.java
+++ pong/Deska.java
@@ -2,8 +2,6 @@ package pong;
 
 import java.awt.Graphics;
 import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
